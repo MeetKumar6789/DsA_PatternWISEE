a28397e3912fc6be09a3446c49462c0447fbdc02
ZeroHochmeister <meet.nakarani@outlook.com>
2026-07-24 23:50:37 +0530

    Time: 1 ms (99.96%), Space: 46.8 MB (82.54%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
+        int res = 0;
         for (int i=0;i<nums.length;i++) {
-            result ^= nums[i];
+            res ^= nums[i];
         }
-        return result;
+        return res;
     }
 }
