d9c8ebc63c4dcbaeb3d0049feb4bbca0a1d8461a
ZeroHochmeister <meet.nakarani@outlook.com>
2026-08-11 22:08:11 +0530

    Time: 0 ms (100%), Space: 50.3 MB (46.96%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int hIndex(int[] citations)
+     {   int n = citations.length;
+        int low = 0 , high = n-1;
+         int res = n;
+        while(high >= low )
+        {
+            int guess = low + (high - low)/2;
+            int minPapers = n - guess;
+           
+            if(citations[guess]>=minPapers)
+            {
+                res = guess;
+                high = guess -1;
+            }
+            else
+            {
+                low = guess + 1;
+            }
+        }
+        return n - res;
+        }
+}
