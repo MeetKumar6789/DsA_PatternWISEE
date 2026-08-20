402eb00728d6708b206f31747f318ca3d1aa59a2
ZeroHochmeister <meet.nakarani@outlook.com>
2026-08-20 22:44:28 +0530

    Time: 6 ms (36.44%), Space: 48.1 MB (22.75%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+             int n = nums[i];
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+
+        int []ans = new int[nums.length];
+        int idx = 0;
+     for(Map.Entry<Integer,Integer>entry:map.entrySet())
+     {
+      int n = entry.getKey();
+      int m = entry.getValue();
+      if(m==1)
+      {
+        ans[idx++] = n;
+      }
+
+     }
+
+     return Arrays.copyOf(ans , idx);      
+    }
+}
