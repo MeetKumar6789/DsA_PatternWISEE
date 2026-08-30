0180bb8c1614247f98846f6f698ce67f94fa9c92
ZeroHochmeister <meet.nakarani@outlook.com>
2026-08-30 14:00:46 +0530

    Time: 5 ms (43.03%), Space: 43 MB (54.98%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,69 +1,78 @@
 class Solution {
-    class Pair
-    {
-        int first;//freq
-        String second; //string;
+    class Pair{
+        int first; //freq of the particular char as the String 
+        String second; //char as the String 
 
-        public Pair(int first , String second )
+        public Pair(int first , String second)
         {
-            this.first= first;
-            this.second = second;
+            this.first = first;
+            this.second = second; 
         }
     }
+   
     public String reorganizeString(String s) {
-        HashMap<Character, Integer>map = new HashMap<>();
+       HashMap<Character,Integer>map = new HashMap<>();
        for(int i=0;i<s.length();i++)
        {
         char c = s.charAt(i);
         map.put(c,map.getOrDefault(c,0)+1);
        }
 
+      
        PriorityQueue<Pair>pq = new PriorityQueue<>(
-   
-              (a,b) -> {
-                if(b.first!=a.first)return b.first - a.first;//max on frequency 
-                return a.second.compareTo(b.second);//min on second
-              }      
+        (a,b) -> {
+            if(b.first!= a.first)
+            {
+                return b.first-a.first; //Max on the freq
+            }
+            return a.second.compareTo(b.second); //Min on the char 
+        }
        );
- for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-    char x = entry.getKey();      // the character
-    int y = entry.getValue();     // its frequency
-    pq.add(new Pair(y, String.valueOf(x)));
-}
 
-     StringBuilder sb = new StringBuilder();
 
-     while(pq.size()>=2)
-     {
-      Pair p1 = pq.poll(); // first most frequent
-      Pair p2 = pq.poll(); //second most frequent
-
-      sb.append(p1.second);
-      sb.append(p2.second);
-
-      p1.first--;
-      p2.first--;
-
-      //till they are zero
-      if(p1.first>0)
-      {
-        pq.add(p1);
+      for(Map.Entry<Character,Integer>entry : map.entrySet()){
+        char x = entry.getKey(); //the real char
+        int y = entry.getValue(); //da real freq of the char 
+        pq.add(new Pair(y , String.valueOf(x)));
       }
-      if(p2.first>0)
-      {
-        pq.add(p2);
-      }
-     }
 
-     if(!pq.isEmpty())
-     {
+     StringBuilder sb =  new StringBuilder();
+
+      while(pq.size()>=2)
+      {
+        //taking the top 2 
+        Pair p1 = pq.poll();
+        Pair p2 = pq.poll();
+
+        sb.append(p1.second); //append the char only 
+        sb.append(p2.second); //append the char only 
+
+         //decrementing the freq
+         p1.first--;
+         p2.first--;
+
+         if(p1.first>0)
+         {
+           pq.add(p1);
+         }
+
+         if(p2.first>0)
+         {
+           pq.add(p2);
+         }
+      }
+
+      if(!pq.isEmpty())
+      {
+        //still one char of the string is there:
         Pair last = pq.poll();
         if(last.first>1)
         {
-            return "";
+            return "";//returning the null string cause if the element at the last have the freq >1 then both of teh last char are gonna be identical 
         }
         sb.append(last.second);
-     }
-     return sb.toString();
+      }
+     
+        return sb.toString();
     }
 }
