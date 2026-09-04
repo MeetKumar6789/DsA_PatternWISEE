8aee98f4e821e99515b0674329333cb808fe89fa
ZeroHochmeister <meet.nakarani@outlook.com>
2026-09-04 16:46:30 +0530

    Time: 8 ms (6.97%), Space: 46.1 MB (16.2%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -10,15 +10,5 @@ class Solution {
         return s.equals(reversed);
     }
 
-    public static void main(String[] args) {
-        Scanner s1 = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int x = s1.nextInt();  
-
-        Solution sol = new Solution();
-        boolean result = sol.isPalindrome(x);
-        System.out.println(result);
-
-        s1.close();
-    }
+    
 }
