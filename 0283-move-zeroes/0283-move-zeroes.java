913d38f01bf6ac94812194042902424b2da8947a
ZeroHochmeister <meet.nakarani@outlook.com>
2026-09-17 00:40:29 +0530

    Time: 2 ms (91.73%), Space: 47.9 MB (38.25%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int i =0;
+        for(int j = 0 ; j<n ; j++)
+        {
+            if(nums[j]!=0)
+            {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+
+         while (i< nums.length) {
+            nums[i] = 0;
+            i++;
+        }
+    }
+}
